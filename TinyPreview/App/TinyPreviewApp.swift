a182333b6cfc197cd82ecf48c7093d47982ecbc7
LingMacker <lingmacker@gmail.com>
2026-08-02 23:14:27 +0800

    关闭窗口退出app

--- TinyPreview/App/TinyPreviewApp.swift
+++ TinyPreview/App/TinyPreviewApp.swift
@@ -1,7 +1,14 @@
 import SwiftUI
 
+final class ApplicationDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
+}
+
 @main
 struct TinyPreviewApp: App {
+    @NSApplicationDelegateAdaptor(ApplicationDelegate.self) private var applicationDelegate
     var body: some Scene {
         WindowGroup("TinyPreview 设置") {
             SettingsView()
